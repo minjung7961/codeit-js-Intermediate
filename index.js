97a8017d1da0119918b3532c8b1efbd01c1a11c7
minjung <alswjd7961@naver.com>
2022-02-07 17:44:01 +0900

    learn about window object

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-// 이벤트(Event) 와 버튼 클릭
-const btn = document.querySelector('#myBtn');
-console.log(btn);
-// 이벤트 핸들링(Event Handling)
-btn.onclick = function () { // 해당 함수를 이벤트 헨들러라 부름
-  console.log("Hello minjung!");
-}
+// window 객체 - 화면에 보여지는 정보에 관한 객체 사실 자바스크립트의 최상위 객체임
+console.log(window);
+console.log(window.innerWidth);
+window.console.log(window.innerHeight); // 모든 객체가 window 객체 안에있으니 맨앞에 window 생략해도 사용가능하다.
