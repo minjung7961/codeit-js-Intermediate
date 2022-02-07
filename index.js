d898a57a425a8e8cb65829cc8d704ac2a280cab0
minjung <alswjd7961@naver.com>
2022-02-07 14:10:28 +0900

    write to console when we used document.getElementBuId()

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-const myNumberTag = document.getElementById('myNumber');
-const decreaseBtn = document.getElementById('decrease');
-const increaseBtn = document.getElementById('increase');
-
-let myNumber = +myNumberTag.textContent;
-
-decreaseBtn.onclick = function() {
-  myNumber--;
-  myNumberTag.textContent = myNumber;
-};
-
-increaseBtn.onclick = function() {
-  myNumber++;
-  myNumberTag.textContent = myNumber;
-};
-
-const colorBtns = document.getElementsByClassName('color-btn');
-
-for (let btn of colorBtns) {
-  btn.onclick = function() {
-    myNumberTag.style.color = btn.dataset.color;
-  };
-}
+// id로 태그 선택하기
+const myTag = document.getElementById("btns");
+console.log(myTag);
