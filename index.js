90e20c83c9a6eac96e5ceb3e78c8e645b911accf
minjung <alswjd7961@naver.com>
2022-02-07 16:54:32 +0900

    event handler's names

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // 이벤트(Event) 와 버튼 클릭
 const btn = document.querySelector('#myBtn');
 console.log(btn);
-btn.onclick = function () {
+// 이벤트 핸들링(Event Handling)
+btn.onclick = function () { // 해당 함수를 이벤트 헨들러라 부름
   console.log("Hello minjung!");
 }
