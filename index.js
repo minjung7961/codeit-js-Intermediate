8772efe6ea1dac18d5cdf05cd08985659f14dace
minjung <alswjd7961@naver.com>
2022-02-07 15:27:01 +0900

    select one or more  tag by css selector

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 // id로 태그 선택하기
-const myTag = document.getElementById("btns");
+document.getElementById("btns");
+
+
+// css 선택자로 태그 선택하기
+const myTag = document.querySelector('.color-btn');
 console.log(myTag);
+
+// css 선택자로 태그 여러개 선택하기
+
+// 1.
+const myTag2 = document.querySelectorAll('.color-btn');
+console.log(myTag2);
+
+// 2.
+const myTag3 = document.getElementsByClassName('color-btn');
+console.log(myTag3);
