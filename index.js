b4815c370d9bf3d1f5e3445d04d69106f9c4b4f7
minjung <alswjd7961@naver.com>
2022-02-07 17:55:19 +0900

    learn what is document object and console.dir

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-// window 객체 - 화면에 보여지는 정보에 관한 객체 사실 자바스크립트의 최상위 객체임
-console.log(window);
-console.log(window.innerWidth);
-window.console.log(window.innerHeight); // 모든 객체가 window 객체 안에있으니 맨앞에 window 생략해도 사용가능하다.
+// Dom (Document Object Model) 문서 객체 모델
+// 브라우저에 나타낸 요소들을 모두 객체로 표현함을 이야기함
+// 브라우저의 최상당 객체는 document
+// document 객체를 이용해 화면의 모든요소를 핸들링할수 있다.
+
+console.log(document);
+console.log("document 의 자료형 : ",typeof document);
+console.log("document의 객체로 출력하기");
+console.dir(document)
+
