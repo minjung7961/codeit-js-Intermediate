f14b8f10865f011a3b73399b861453d4075b1a05
minjung <alswjd7961@naver.com>
2022-02-08 16:57:30 +0900

    how to add node elements

--- index.js
+++ index.js
@@ -12,7 +12,27 @@ console.log(document);
 const today2 = document.querySelector('#today'); // 바뀌어버렸기 때문에 새로 요소를 찾아야함 (이 말인 즉슨 실수를 자주 유발하게 된다.)
 today2.outerHTML =  today2.outerHTML + '<p>다음</p>';
 
-// // 요소 노드 추가하기
-// const tomorrow = document.querySelector('#tomorrow');
+// 요소 노드 추가하기
+const tomorrow = document.querySelector('#tomorrow');
 
-// // 1. 요소 노드 만들기 : document
+// 1. 요소 노드 만들기 : document.createElement('태그이름')
+const first = document.createElement('li');
+
+// 2. 요소 노드 꾸미기 : textContent, innerHTML, ...
+first.textContent = '처음';
+
+// 3. 요소 노드 추가하기 : NODE.prepend, append, after, before
+tomorrow.prepend(first); // 해당 노드의 젤 첫번째 자식노드로 추가
+
+
+const last = document.createElement('li');
+last.textContent = '마지막';
+tomorrow.append(last); // 해당 노드의 젤 마지막 자식노드로 추가
+
+const prev = document.createElement('p');
+prev.textContent = '이전';
+tomorrow.before(prev);// 해당 노드의 앞에 형제 노드를 추가
+
+const next = document.createElement('p');
+next.textContent = '다음';
+tomorrow.after(next);// 해당 노드의 뒤에 형제 노드를 추가
