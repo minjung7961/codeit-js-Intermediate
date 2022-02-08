9fa05589721fa62882e59f56a258efd2589742da
minjung <alswjd7961@naver.com>
2022-02-08 17:18:31 +0900

    how to move node element

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const today = document.querySelector('#today');
 const tomorrow = document.querySelector('#tomorrow');
 
-// 노드 삭제하기: Node.remove();
-tomorrow.remove();
-today.children[2].remove();
+// 노드  이동하기: prepend, append, before, after
+
+// 오늘 할일의 맨뒤에 내일할일의 뮤지컬 공연 예매를 추가해보자
+today.append(tomorrow.children[1]); // 이동!
+console.log(document);
