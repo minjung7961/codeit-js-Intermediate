7858274ef4dc1288ba6d6d584133fa04363b9131
minjung <alswjd7961@naver.com>
2022-02-08 11:57:56 +0900

    how to access element node in js

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-// Dom (Document Object Model) 문서 객체 모델
-const title = document.querySelector('#title') // title 아이디를 가진 태그를 가져오라
+// Don 트리 여행하기
+const myTag = document.querySelector('#list-1');
 
-// 브라우저 띄우고 title 변수가 담고있는 id 가 title 인 객체에 접근가능
-// 브라우저 콘솔창에 title.style.color = 'red' 실습 해보기
+// 부모 요소 노드 접근 방법
+console.log('부모 요소 접근방법');
+console.log(myTag.parentElement);
+
+// 형제 요소 노드 접근 방법
+console.log('형제 요소 접근방법');
+console.log(myTag.previousElementSibling);
+console.log(myTag.nextElementSibling);
+
+// 자식 요소 노드 접근 방법
+console.log('자식 요소 접근방법');
+console.log(myTag.children); // html 컬렉션 형태로 자식 노드들 출력
+console.log(myTag.children[0]);
+console.log(myTag.children[1]);
+console.log(myTag.children[2]);
+console.log(myTag.children[3]);
+console.log(myTag.children['list-1']);
+console.log('첫번째 마지막 요소 접근방법');
+console.log(myTag.firstElementChild);
+console.log(myTag.lastElementChild);
+
+//  접근 연결해서도 가능
+console.log(myTag.parentElement.nextElementSibling);
