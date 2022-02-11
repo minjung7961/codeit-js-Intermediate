986c64566d307e923540cfbcbf2ce65d568babac
minjung <alswjd7961@naver.com>
2022-02-11 17:37:48 +0900

    handling tag's attribute with node's method - node.getAttribute()

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const tomorrow = document.querySelector('#tomorrow');
 const item = tomorrow.firstElementChild;
 const link = item.firstElementChild;
 
+// 요소노드의 매소드를 이용해서 html 태그 가 가지고있는 속성을 핸들링하는것을 배워보자
+
+//  // elem.getAttribute('속성'); 속성에 접근하기
+console.log(tomorrow.getAttribute('href'));
+console.log(item.getAttribute('class')); // 이름 살짝 바뀜 유의 22
+
 // id 속성
 console.log(tomorrow);
 console.log(tomorrow.id);
@@ -15,3 +21,4 @@ console.log(item.className); // 이름 살짝 바뀜 유의
 console.log(link);
 console.log(link.href);
 console.log(tomorrow.href); // undefine : html 표준이아니기때문
+
