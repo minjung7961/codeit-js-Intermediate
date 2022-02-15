152b4eec7804059bae8af5e3b630373cf8c14ac8
minjung <alswjd7961@naver.com>
2022-02-15 17:34:36 +0900

    console status when we use element's classList method

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ const tomorrow = document.querySelector('#tomorrow');
 
 // 따라서 해당 요소의 프로퍼티를 변경하기보단 클래스를 선택해 변경하는것을 권장함
 
-//  // elem.chlassName
+//  // elem.classList:
+console.log(today.classList); // -> DOMTokenList(2) ['list', 'today',  value: 'list today']
+console.log(today.children[1].classList); // -> DOMTokenList ['item', value: 'item]
+
+//  // elem.chlassName: add, remove, toggle
 today.children[1].className = 'done';
 console.log(today.children[1]);
 
 const done = document.getElementsByClassName('done');
-console.dir(done);
-
+// 기존의 class 이름 item 이 사라지는데 안사라졌음좋겠다? -> classList를 사용하쟈
 console.log(document)
 
 // style 프로퍼티
