5a2af948d41704ccb5cf29564a21076e27659ffe
minjung <alswjd7961@naver.com>
2022-02-15 11:21:47 +0900

    how to remove node's attribute by js

--- index.js
+++ index.js
@@ -14,3 +14,11 @@ console.log(item.getAttribute('class')); // 이름 살짝 바뀜 유의 22
 // // elem.setAttribute('속성', '값'); 속성 추가(수정)하기
 tomorrow.setAttribute('class','list'); // 추가
 link.setAttribute('href',"htpps://www.codeit.kr")// 수정
+
+// 요소 노드의 속성을 제거하는 방법
+
+// // elem.removeAttribute('속성'): 속성 제거하기
+console.log(tomorrow);
+tomorrow.removeAttribute('href');
+tomorrow.removeAttribute('class');
+console.log(tomorrow);
