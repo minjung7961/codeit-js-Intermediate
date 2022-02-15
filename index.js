207a7c72c5ed0d54541dcc94926677b18f21ddf9
minjung <alswjd7961@naver.com>
2022-02-15 17:13:14 +0900

    confirm method - is run well?

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const tomorrow = document.querySelector('#tomorrow');
 today.children[1].className = 'done';
 console.log(today.children[1]);
 
+const done = document.getElementsByClassName('done');
+console.dir(done);
+
+console.log(document)
+
 // style 프로퍼티
 today.children[0].style.textDecoration = 'line-through';
 today.children[0].style.backgroundColor = '#DDDDDD';
