b9a9260d6e39cc28cc434c68e2ffdb660776f2c8
minjung <alswjd7961@naver.com>
2022-02-16 15:11:05 +0900

    Precautions when using eventHandlers

--- index.js
+++ index.js
@@ -16,3 +16,17 @@ btn.addEventListener('click',event2);
 
 // 2. elem.removeEventListener(event, handler)
 btn.removeEventListener('click', event2)
+
+// removeEventListener 사용시 주의할 점
+// 1. 등록했던 함수의 이름과 반드시 같아야함
+// ex
+
+btn.addEventListener('click', function(){
+  console.log('event3!');
+});
+
+// 제대로 작동안함 이유는 등록했던 바로위의 함수의 이름과 다르기때문(무명 함수...)
+// 무명함수 못지우니 이름명시한 함수형태로 등록하고 지우도록하자!
+btn.removeEventListener("click", function(){
+  console.log('event3!')
+});
