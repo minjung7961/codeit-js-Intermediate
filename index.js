d8bd0db175886c7b77e26bc49c278e3eb03558c6
minjung <alswjd7961@naver.com>
2022-02-16 17:16:11 +0900

    example how to use EventObject

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const myBtn = document.querySelector('#myBtn');
 
 // 함수에 이벤트 객체를 파라미터로 전달. 이벤트 객체는 이벤트 발생시 자동으로 만들어진다.
 function printEvent(event){
-  console.log(event);
+  console.log(event); // 여러 프로퍼티를 출력하는데 그중에 target 과 type 을 자주사용한다 아래와같이
+  event.target.style.color = 'red'; // 이벤트가 발생한 요소의 태그의 스타일을 변경
 }
 
 myInput.addEventListener('keydown', printEvent);
