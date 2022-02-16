833e286d020a63d5b56c04eaa722809c77dcddda
minjung <alswjd7961@naver.com>
2022-02-16 15:00:32 +0900

    learn what is addEventListener

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // 이벤트 핸들러 등록하기
 let btn = document.querySelector('#myBtn');
 
-// 기존 html 의 onclick 프로퍼티에 덮어써서 기능함
-btn.onclick = function () {
+// 이벤트 핸들러가 덮어씌어져 사라지지 않도록 addEventlistener(event, handler); 사용하기
+function event1() {
   console.log('Hi Codeit!');
 };
 
-const myContent = document.querySelector('#content');
-myContent.innerHTML = '<div>Hello</div>'; //위의 이벤트 코드위에도 덮어쓰는바람에 hello 만 남음
-myContent.className = 'button';
+function event2(){
+  console.log("Hi again!");
+};
+
+btn.addEventListener('click',event1);
+btn.addEventListener('click',event2);
