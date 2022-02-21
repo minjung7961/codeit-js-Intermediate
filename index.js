67b08b322f7562bd9e7383bd13b6a708586a01c9
minjung <alswjd7961@naver.com>
2022-02-22 00:27:19 +0900

    how to solve front commit's problom -> event Delegation

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ list.addEventListener('click', function(e){
   e.target.classList.toggle('done'); 
 })
 
-// 만약 객체를 새로 만든다면 추가한 요소에 이벤트 헨들러가 동작하지 않는다는문제가 생긴다. 동작하게 하려면 일일이 추가해줘야한다 이는 코드 가독성이나 유지보수하기 좋지 않다.
+// 새로생긴 자식객체에 이벤트가 없지만 부모요소에 이벤트 핸들러가 걸려있기때문에 자동으로 자식요소 클릭시 이벤트 핸들러가 잘 작동한다.
 const li = document.createElement('li');
 li.classList.add('item');
 li.textContent = '일기 쓰기';
