3881b9ee32cc25447d80b6f2d20efe688a6f4586
minjung <alswjd7961@naver.com>
2022-02-21 16:58:55 +0900

    what is event bubbling

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-// 이벤트 객체(Event Object)
-const myInput = document.querySelector('#myInput');
-const myBtn = document.querySelector('#myBtn');
+// 이벤트 버블링 (Event Bubbling)
+const content = document.querySelector('#content');
+const title = document.querySelector('#title');
+const list = document.querySelector('#list');// 아이디값 기준으로 선택할때 이름 #으로 시작
+const items = document.querySelectorAll('.item'); // 클래스기준으로 선택할떄 이름 . 으로 시작 all 메소드사용
 
-// 이전 커밋처럼 핸들링 하기 보다는 각 이벤트의 함수에 상세한 설명이 필요하다.
+content.addEventListener('click', function(){
+  console.log('content Event');
+});
 
-// 함수에 이벤트 객체를 파라미터로 전달. 이벤트 객체는 이벤트 발생시 자동으로 만들어진다.
-function printEvent(event){
-  console.log(event); // 여러 프로퍼티를 출력하는데 그중에 target 과 type 을 자주사용한다 아래와같이
-  event.target.style.color = 'red'; // 이벤트가 발생한 요소의 태그의 스타일을 변경
+title.addEventListener('click', function(){
+  console.log('title Event');
+});
+
+list.addEventListener('click', function(){
+  console.log('list Event');
+})
+
+for(let item of items){
+  item.addEventListener('click', function(){
+    console.log('item Event');
+  });
 }
-
-myInput.addEventListener('keydown', printEvent);
-myBtn.addEventListener('click', printEvent);
