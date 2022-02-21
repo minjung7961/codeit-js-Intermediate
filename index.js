3d6de7ed77ac8439f40306098dd665a636951c63
minjung <alswjd7961@naver.com>
2022-02-22 00:37:37 +0900

    how to solve event Delegation's provlem?

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const list = document.querySelector('#list');
 
 // 아직 문제는 남아있다 자식요소 각각에만 이벤트 헨들링을 하고싶지만 부모요소를 클릭해도 이벤트가 핸들링되게된다 -> 자식요소들모두에게 이벤트 핸들러가 작동하게된다
 list.addEventListener('click', function(e){
-  e.target.classList.toggle('done'); 
-})
+  // solution : class 이름에 item 이있는지 여부를 boolean 형태로 반환하는 메소드를 이용해 그 조건이 맞을때만 작동하게끔 구현한다
+  if(e.target.classList.contains('item')){
+    e.target.classList.toggle('done');
+  }
+});
 
 
 const li = document.createElement('li');
