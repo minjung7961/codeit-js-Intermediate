505040897e7128bd3329f6e5c6e5d83ec19c88af
minjung <alswjd7961@naver.com>
2022-02-21 17:12:55 +0900

    how to know eventHanling parents node

--- index.js
+++ index.js
@@ -4,26 +4,27 @@ const title = document.querySelector('#title');
 const list = document.querySelector('#list');// 아이디값 기준으로 선택할때 이름 #으로 시작
 const items = document.querySelectorAll('.item'); // 클래스기준으로 선택할떄 이름 . 으로 시작 all 메소드사용
 
-content.addEventListener('click', function(){
+content.addEventListener('click', function(e){
   console.log('content Event');
+  // 과연 이벤트 버블링 일어나는 부모 요소들까지 출력할까? 
+  // -> 아니다 해당 요소만 출력한다. 
+  // 덕분에 이벤트 핸들러 버블링 일어나는 최초의 위치를 알수있게 된다.
+  console.log(e.currentTarget); 
 });
 
-title.addEventListener('click', function(){
+title.addEventListener('click', function(e){
   console.log('title Event');
+  console.log(e.currentTarget);
 });
 
-list.addEventListener('click', function(){
+list.addEventListener('click', function(e){
   console.log('list Event');
+  console.log(e.currentTarget);
 })
 
 for(let item of items){
-  item.addEventListener('click', function(){
+  item.addEventListener('click', function(e){
     console.log('item Event');
+    console.log(e.currentTarget);
   });
 }
-
-// 이벤트 헨들링이란?
-
-// 객체가 선택되었을때  객체의 이벤트 핸들러만 실행되는게아니라 부모요소의 이벤트 헨들러까지
-// 실행되는것을 이야기한데 이게 부모의 부모 올라가다가 윈도우객체만날때까지 반복된다. 코드는 이전
-// 커밋을 보자
