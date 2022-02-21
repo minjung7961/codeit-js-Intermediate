f6ade5ca34a174879931813313d1adf6ba191409
minjung <alswjd7961@naver.com>
2022-02-22 00:31:24 +0900

    what is event Delegation's probluem?

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // 이벤트 위임 event Delegation
 const list = document.querySelector('#list');
 
-// 부모요소는 이벤트 버블링을 통해 자식노드들의 이벤트 발생사실을 알게되니 이를 이용해 부모에만 이벤트를 등록하고 이벤트 객체에는 이벤트 위치정보를 가지고 있으니 이를 이용하면 앞선문제 해결된다.
+// 아직 문제는 남아있다 자식요소 각각에만 이벤트 헨들링을 하고싶지만 부모요소를 클릭해도 이벤트가 핸들링되게된다 -> 자식요소들모두에게 이벤트 핸들러가 작동하게된다
 list.addEventListener('click', function(e){
   e.target.classList.toggle('done'); 
 })
 
-// 새로생긴 자식객체에 이벤트가 없지만 부모요소에 이벤트 핸들러가 걸려있기때문에 자동으로 자식요소 클릭시 이벤트 핸들러가 잘 작동한다.
+
 const li = document.createElement('li');
 li.classList.add('item');
 li.textContent = '일기 쓰기';
