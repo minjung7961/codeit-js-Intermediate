bea1a61bc81ddbc7cb93d2f34b445b722dbf46c6
minjung <alswjd7961@naver.com>
2022-02-22 00:25:54 +0900

    how to solve front commit's problom -> event Delegation

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 // 이벤트 위임 event Delegation
 const list = document.querySelector('#list');
 
-// for / of 문 : 객체의 모든 열거할 수 있는 프로퍼티의 개수만큼 반복적으로 실행하고자 하는 실행문;
-for(let item of list.children){
-  item.addEventListener('click', function(e){
-    e.target.classList.toggle('done');
-  });
-}
+// 부모요소는 이벤트 버블링을 통해 자식노드들의 이벤트 발생사실을 알게되니 이를 이용해 부모에만 이벤트를 등록하고 이벤트 객체에는 이벤트 위치정보를 가지고 있으니 이를 이용하면 앞선문제 해결된다.
+list.addEventListener('click', function(e){
+  e.target.classList.toggle('done'); 
+})
 
 // 만약 객체를 새로 만든다면 추가한 요소에 이벤트 헨들러가 동작하지 않는다는문제가 생긴다. 동작하게 하려면 일일이 추가해줘야한다 이는 코드 가독성이나 유지보수하기 좋지 않다.
 const li = document.createElement('li');
